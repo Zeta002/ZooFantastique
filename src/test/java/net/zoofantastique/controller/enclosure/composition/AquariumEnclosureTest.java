9de5d12efaff6c8cf8ae17e03dd7b10de67d1d34
Zeta002 <anngeloportelli05@gmail.com>
2023-11-23 17:28:21 +0100

    Début test aquarium, à terminer

---
+++ src/test/java/net/zoofantastique/controller/enclosure/composition/AquariumEnclosureTest.java
@@ -0,0 +1,16 @@
+package net.zoofantastique.controller.enclosure.composition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AquariumEnclosureTest {
+
+    @Test
+    void addCreature() {
+    }
+
+    @Test
+    void maintenance() {
+    }
+}
